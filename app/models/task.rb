672c8d1f4d0892714d887dfd74bde7c2f8f19144
Ilia <teterailiya@gmail.com>
2024-09-23 16:54:50 +0300

    Add atributes to Task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ApplicationRecord
+  enum status: { undone: 0, done: 10 }
+  enum priority: { regular: 0, medium: 10, extra: 20 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_23_125950) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_23_130220) do
   create_table "columns", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -26,6 +26,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_23_125950) do
   create_table "tasks", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "description"
+    t.integer "status", default: 0
+    t.integer "priority", default: 0
+    t.datetime "deadline"
   end
 
   create_table "users", force: :cascade do |t|
