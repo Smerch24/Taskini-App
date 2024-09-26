17e2833f5eedd5f77c72f5a622a37d4f37b06ccf
Ilia <teterailiya@gmail.com>
2024-09-26 12:32:10 +0300

    Make spec tests for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :tables, dependent: :destroy
 
   validates :name, presence: true
+  validates :name, length: { minimum: 1, maximum: 32 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
