9d1f182aa73b72b6945bc59a4b50c0f82b6d654c
Ilia <teterailiya@gmail.com>
2024-10-01 18:52:39 +0300

    Update task logic

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,29 +8,40 @@ class TasksController < ApplicationController
 
   def new
     @task = Task.new
-    @columns = Column.all
+    @column = Column.find(params[:column_id]) if params[:column_id]
+    @columns = Column.where(table_id: current_user.tables.pluck(:id))
   end
 
   def create
-    @columns = Column.all
-    @task = Task.new(task_params)
-    if @task.save
-      redirect_to tables_path
+    @column = Column.find(params[:task][:column_id])
+    table = @column.table
+
+    # Проверка, что текущий пользователь является владельцем таблицы
+    if table.user_id == current_user.id
+      @task = @column.tasks.new(task_params)
+
+      if @task.save
+        redirect_to table_path(table), notice: 'Задание успешно создано'
+      else
+        render :new, alert: 'Ошибка при создании задания'
+      end
     else
-      render :new
+      redirect_to tables_path, alert: 'Вы не можете создавать задания для этой таблицы.'
     end
   end
 
   def edit
-    @columns = Column.all
     @task = Task.find(params[:id])
+    @column = Column.find(@task.column_id)
+    @table = current_user.tables.find(@column.table_id)
+    @columns = @table.columns
   end
 
   def update
     @columns = Column.all
     @task = Task.find(params[:id])
     if @task.update(task_params)
-      redirect_to @task
+      redirect_to tables_path
     else
       render :edit
     end
@@ -38,17 +49,18 @@ class TasksController < ApplicationController
 
   def show
     @task = Task.find(params[:id])
+    @columns = current_user.tables.find(params[:id]).columns
   end
 
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
-    redirect_to tasks_path
+    redirect_to tables_path
   end
 
   private
 
   def task_params
-    params.require(:task).permit(:name, :column_id)
+    params.require(:task).permit(:name, :description, :column_id, :status, :priority, :deadline)
   end
 end
