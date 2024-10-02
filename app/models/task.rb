4c1731def8f1670719510baa84de0838ea758202
Ilia <teterailiya@gmail.com>
2024-10-02 11:53:59 +0300

    Change model specs

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,8 @@ class Task < ApplicationRecord
   validates :name, presence: true
   validates :status, presence: true
   validates :priority, presence: true
+  validates :description, length: { maximum: 150 }
+  validates :name, length: { minimum: 2, maximum: 30 }
 
   enum status: { undone: 0, done: 10 }
   enum priority: { regular: 0, medium: 10, extra: 20 }
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -9,8 +9,7 @@ RSpec.describe Task, type: :model do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:status) }
     it { should validate_presence_of(:priority) }
-    it { should validate_length_of(:description).is_at_least(2) }
-    it { should validate_length_of(:description).is_at_most(120) }
+    it { should validate_length_of(:description).is_at_most(150) }
     it { should validate_length_of(:name).is_at_least(2) }
     it { should validate_length_of(:name).is_at_most(30) }
     it { should define_enum_for(:status) }
